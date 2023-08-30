1f45b685d47ca3caa30185f5dad536c789b4737d
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-08-30 23:25:28 +0700

    demo post page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../resources/templates/**/*.{html,js}"], // it will be explained later
+    content: ["./src/main/resources/templates/**/*.{html,js}"], // it will be explained later
     theme: {
         extend: {},
     },
