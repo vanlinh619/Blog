92c61cf880ee34837f338d4ed4bd7e7ed84f2926
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-09-03 15:37:51 +0700

    remove css

--- src/main/java/com/ale/blog/BlogApplication.java
+++ src/main/java/com/ale/blog/BlogApplication.java
@@ -2,12 +2,11 @@ package com.ale.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
-
+public class BlogApplication extends SpringServletContainerInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
-
 }
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/main/resources/templates/**/*.{html,js}"], // it will be explained later
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
