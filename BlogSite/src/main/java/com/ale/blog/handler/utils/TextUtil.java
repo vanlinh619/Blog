3a55021569569fc44d96772bf8a5a04a4810ddbf
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-10-27 21:03:58 +0700

    show post of category

--- BlogSite/src/main/java/com/ale/blog/handler/utils/TextUtil.java
+++ BlogSite/src/main/java/com/ale/blog/handler/utils/TextUtil.java
@@ -7,6 +7,7 @@ public class TextUtil {
     public static final String HOME = "Trang chủ";
     public static final String DOCUMENT = "Tài liệu";
     public static final String ALL_DOCUMENT = "Tất cả tài liệu";
+    public static final String ALL_CATEGORY = "Tất cả bài viết";
 
 
     public static String getLinkAllDocument(User author) {
--- BlogSite/src/main/java/com/ale/blog/service/PostService.java
+++ BlogSite/src/main/java/com/ale/blog/service/PostService.java
@@ -3,8 +3,10 @@ package com.ale.blog.service;
 import com.ale.blog.entity.Category;
 import com.ale.blog.entity.Post;
 import com.ale.blog.entity.User;
+import com.ale.blog.entity.state.PostState;
 import com.ale.blog.handler.mapper.pojo.request.PostRequest;
 import com.ale.blog.handler.mapper.pojo.request.QueryRequest;
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import org.springframework.data.domain.Page;
 
@@ -14,7 +16,20 @@ public interface PostService extends EntityService<Post, Long> {
     Post createPostArticle(PostRequest postRequest, User author);
     Post getPostBySlug(String slug, @Nullable User owner);
     Page<Post> findAllByAuthor(User author, QueryRequest queryRequest);
+    Page<Post> findAllByAuthor(
+            @Nullable User owner,
+            @Nonnull User author,
+            @Nonnull PostState state,
+            @Nonnull QueryRequest queryRequest
+    );
     Page<Post> findAllByCategory(Category category, QueryRequest queryRequest);
+    Page<Post> findAllByCategory(
+            @Nonnull Category category,
+            @Nullable User owner,
+            @Nonnull User author,
+            @Nonnull PostState state,
+            @Nonnull QueryRequest queryRequest
+    );
 
     //Api authorize
     Post getByIdAndAuthor(Long id, User author);
